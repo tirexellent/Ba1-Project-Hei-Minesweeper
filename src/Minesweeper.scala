15d91ebaa74bbb4225f67963ea565a3f9eca76b0
Se7en <seven.raymond@epfl.ch>
2023-12-19 13:48:40 +0100

    first commit + emptygrid for easy mode

---
+++ src/Minesweeper.scala
@@ -0,0 +1,8 @@
+object Minesweeper extends App {
+  def emptyGrid () : Array[Array[Int]]= {
+    var line : Array[Int]= Array.fill(9)(0)
+    var column : Array[Array[Int]] = Array.fill(9)(line)
+    column
+  }
+
+}
