915ffdc861786b61e99f591d039b9bc13a9c2ac2
Se7en <seven.raymond@epfl.ch>
2023-12-20 17:12:09 +0100

    first commit + grid for easy mode (9x9)

--- src/Minesweeper.scala
+++ src/Minesweeper.scala
@@ -3,7 +3,29 @@ object Minesweeper extends App {
     override def toString: String = s"($isMine,$count,$isVisible)"
   }
 
+  def createArray(rows: Int, cols: Int): Array[Array[Cell]] = {
+    // Initialize cells with default values
+    var array = Array.ofDim[Cell](rows, cols)
+    for (i <- 0 until rows) {
+      for (j <- 0 until cols) {
+        array(i)(j) = new Cell(false, 0, false)
 
+      }
+    }
+    array
+  }
+
+  val rows = 9
+  val cols = 9
+  var fullArea: Array[Array[Cell]] = createArray(rows, cols)
+
+
+  for (row <- fullArea) {
+    for (cell <- row) {
+      print(cell + " ")
+    }
+    println()
+  }
 
 
 }
