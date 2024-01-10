3c5afcc7a8ffde2bb49aa2f1b0a975c0488cfd71
Se7en <seven.raymond@epfl.ch>
2024-01-10 22:43:20 +0100

    small modifications, found way to flood (implement in areaSafe)

--- src/Minesweeper.scala
+++ src/Minesweeper.scala
@@ -4,6 +4,9 @@ object Minesweeper extends App {
   class Cell(var isMine: Boolean, var count: Int, var isVisible: Boolean, var flag: Boolean) {
     override def toString: String = s"($isMine, $count, $isVisible, $flag) "
   }
+  class safeCells (var isSafe : Int){
+    override def toString: String = s"($isSafe) "
+  }
 
   def createArray(rows: Int, cols: Int): Array[Array[Cell]] = {
     // Initialize cells with default values
@@ -17,10 +20,22 @@ object Minesweeper extends App {
     array
   }
 
+  def safeArray(rows: Int, cols: Int): Array[Array[safeCells]] = {
+    var array = Array.ofDim[safeCells](rows, cols)
+    for (i <- 0 until rows) {
+      for (j <- 0 until cols) {
+        array(i)(j) = new safeCells(0)
+
+      }
+    }
+    array
+  }
+
   val rows: Int = 10
   val cols: Int = 10
   var nbmines : Int = 10
   var fullArea: Array[Array[Cell]] = createArray(rows, cols)
+  var safeSpace: Array[Array[safeCells]] = safeArray(rows, cols)
   var i : Int = 0
 
 
@@ -61,8 +76,17 @@ object Minesweeper extends App {
   }
 
 
-  def areaSafe (x: Int,y: Int): Unit = {
-
+  def areaSafe (): Unit = {
+    for (row <- fullArea){
+      for (cell <- row){
+        if ( cell.count !=0){
+          safeSpace(row)(cell) = -1
+        }
+        else {
+          safeSpace(row)(cell) = 0
+        }
+      }
+    }
 
   }
 
@@ -89,7 +113,8 @@ object Minesweeper extends App {
     println("Choose a cell y pos: ")
     var uncovercelly = Input.readInt() - 1
 
-    if (nbturn == 0){
+    if (fullArea(uncovercellx)(uncovercellx)==0){
+      areaSafe()
     }
     println("reveal or put flag? r/f")
     var flag: Char = Input.readChar()
