704650de889983ba9a9b510a68bec055b726023f
Se7en <seven.raymond@epfl.ch>
2024-01-15 17:07:24 +0100

    full game + ending (win / lose) + remove flag

--- src/Minesweeper.scala
+++ src/Minesweeper.scala
@@ -113,22 +113,41 @@ object Minesweeper extends App {
     }
   }
 
+  def win(): Boolean = {
+    var win : Boolean = false
+    var count : Int = 0
+
+    for (row <- fullArea) {
+      for (cell <- row) {
+        if (!cell.isMine && cell.isVisible) {
+          count += 1
+        }
+      }
+    }
+    if (count == ((rows * cols)-nbmines)){
+      win = true
+    }
+    win
+  }
+
   val rows: Int = 10
   val cols: Int = 10
   var nbmines : Int = 10
   var fullArea: Array[Array[Cell]] = createArray(rows, cols)
   var nbturn : Int =1
+  var uncvrx: Int = 0
+  var uncvry: Int = 0
+  var flag: Char ='0'
   placeMines()
 
-
-  while (true) {
-    for (i<-0 to 10){
+  var game : Boolean = true
+  while (game) {
+    for (i<-0 to 10) {
       println("\n")
     }
-
     for (row <- fullArea) {
       for (cell <- row) {
-        if (cell.isVisible && cell.flag) {
+        if (cell.flag) {
           print("🚩")
         }
         else if (!cell.isVisible) {
@@ -140,14 +159,22 @@ object Minesweeper extends App {
       }
       println()
     }
-    println("\nChoose a cell's x pos: ")
-    val uncvrx = Input.readInt() - 1
+    if (game) {
+      println("\nChoose a cell's x pos: ")
+      uncvrx = Input.readInt() - 1
 
-    println("Choose a cell's y pos: ")
-    val uncvry = Input.readInt() - 1
+      println("Choose a cell's y pos: ")
+      uncvry = Input.readInt() - 1
 
-    println("reveal or put flag? r/f")
-    var flag: Char = Input.readChar()
+      if (fullArea(uncvrx)(uncvry).flag){
+        println("remove flag? y/n")
+      }
+      else {
+        println("reveal or put flag? r/f")
+      }
+      flag = Input.readChar()
+    }
+
 
     if (flag == 'r') {
       fullArea(uncvrx)(uncvry).isVisible = true
@@ -155,12 +182,21 @@ object Minesweeper extends App {
       if (nbturn == 1 || fullArea(uncvrx)(uncvry).count == 0) {
         uncover(uncvrx, uncvry)
       }
-
+      else if (fullArea(uncvrx)(uncvry).isMine && fullArea(uncvrx)(uncvry).isVisible) {
+        println("You hit a mine! Game over!")
+        game = false
+      }
     }
     else if (flag == 'f'){
-      fullArea(uncvrx)(uncvry).isVisible = true
       fullArea(uncvrx)(uncvry).flag = true
     }
+    else if (flag == 'y'){
+      fullArea(uncvrx)(uncvry).flag = false
+    }
     nbturn += 1
+    if (win()) {
+      println("Congratulations! You won!")
+      game = false
+    }
   }
 }
