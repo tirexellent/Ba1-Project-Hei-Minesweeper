5609e57cabb57cb7c564baceb4bd2a704d9df729
Se7en <seven.raymond@epfl.ch>
2024-01-16 01:27:59 +0100

    v1.0

--- src/Minesweeper.scala
+++ src/Minesweeper.scala
@@ -1,16 +1,30 @@
 import hevs.graphics.FunGraphics
 import java.awt.event.{MouseAdapter, MouseEvent}
-import java.awt.Color
+import java.awt.{Color}
 
 object Minesweeper extends App {
+  //all the variables
+  val rows: Int = 10
+  val cols: Int = 10
+  var nbmines: Int = 10
+  var nbturn: Int = 1
+  var uncvrx: Int = 0
+  var uncvry: Int = 0
+  var posx: Int = 0
+  var posy: Int = 0
+  var Button: Int = 0
+
+
+  val gamewindow = new FunGraphics(645, 645, "Ba1 Project, Minesweeper Se7en")
+
   // Initialise class cell to get enough info in every cell
   class Cell(var isMine: Boolean, var count: Int, var isVisible: Boolean,var isSafe : Int , var flag: Boolean) {
     override def toString: String = s"($isMine, $count, $isVisible, $isSafe, $flag) "
   }
 
+  // Initialize cells with default values
   def createArray(rows: Int, cols: Int): Array[Array[Cell]] = {
-    // Initialize cells with default values
-    var array = Array.ofDim[Cell](rows, cols)
+    val array = Array.ofDim[Cell](rows, cols)
     for (i <- 0 until rows) {
       for (j <- 0 until cols) {
         array(i)(j) = new Cell(false, 0, false,0,false)
@@ -19,11 +33,13 @@ object Minesweeper extends App {
     array
   }
 
+  //Ensures the position of the mines in the right amount
+  //+ determines the number of each tile based on the neighboring mine-s
   def placeMines(): Unit = {
     var i : Int = 0
     while (i < nbmines) {
-      var x = (math.random() * 10).toInt
-      var y = (math.random() * 10).toInt
+      val x = (math.random() * 10).toInt
+      val y = (math.random() * 10).toInt
       if (!fullArea(x)(y).isMine) {
         fullArea(x)(y).isMine = true
         i += 1
@@ -49,59 +65,7 @@ object Minesweeper extends App {
     }
   }
 
-  def safe(x: Int, y: Int, distFromCell: Int): Unit = {
-    for (row <- fullArea) {
-      for (cell <- row) {
-        if (cell.isSafe == distFromCell) {
-          for (offset <- -1 to 1 if offset != 0){
-            var newX = x + offset
-            var newY = y + offset
-            if (newX<=rows-1 && newX>=0 ){
-              fullArea(newX)(y).isVisible = true
-              if (fullArea(newX)(y).isSafe == 0){
-                fullArea(newX)(y).isSafe = distFromCell + 1
-                safe(newX,y, distFromCell+1)
-              }
-            }
-            if (newY<=cols-1 && newY>=0 ){
-              fullArea(x)(newY).isVisible = true
-              if (fullArea(x)(newY).isSafe == 0) {
-                fullArea(x)(newY).isSafe = distFromCell + 1
-                safe(x,newY,distFromCell+1)
-              }
-            }
-            if ((x-1)>= 0 && y-1 >=0){
-              fullArea(x-1)(y-1).isVisible = true
-              if (fullArea(x-1)(y-1).count == 0 && !fullArea(x-1)(y-1).isVisible){
-                safe(x-1,y-1,distFromCell)
-              }
-            }
-            if ((x-1) >= 0 && y+1 <= 9) {
-              fullArea(x-1)(y+1).isVisible = true
-              if (fullArea(x-1)(y+1).count == 0 && !fullArea(x-1)(y+1).isVisible) {
-                safe(x-1, y+1, distFromCell)
-              }
-            }
-            if ((x+1) <= 9 && y-1 >= 0) {
-              fullArea(x+1)(y-1).isVisible = true
-              if (fullArea(x+1)(y-1).count == 0 && !fullArea(x+1)(y-1).isVisible) {
-                safe(x+1, y-1, distFromCell)
-              }
-            }
-            if ((x+1) <= 9 && y+1 <= 9) {
-              fullArea(x+1)(y+1).isVisible = true
-              if (fullArea(x+1)(y+1).count == 0 && !fullArea(x+1)(y+1).isVisible) {
-                safe(x+1, y+1, distFromCell)
-              }
-            }
-
-          }
-
-        }
-      }
-    }
-  }
-
+  //On the first turn the first discovered tile needs to be a 0
   def uncover(uncvrx: Int, uncvry : Int): Unit ={
     if (fullArea(uncvrx)(uncvry).isSafe == 0) {
       safe(uncvrx, uncvry,0)
@@ -116,6 +80,94 @@ object Minesweeper extends App {
     nbturn += 1
   }
 
+  //If a 0 is discovered, all the neighboring tiles until the first one with a number should be discovered (all the "safe" tiles)
+  def safe(x: Int, y: Int, distFromCell: Int): Unit = {
+    for (row <- fullArea) {
+      for (cell <- row) {
+        if (cell.isSafe == distFromCell) {
+          for (offset <- -1 to 1 if offset != 0) {
+            val newX = x + offset
+            val newY = y + offset
+            if (newX <= rows - 1 && newX >= 0) {
+              fullArea(newX)(y).isVisible = true
+              if (fullArea(newX)(y).isSafe == 0) {
+                fullArea(newX)(y).isSafe = distFromCell + 1
+                safe(newX, y, distFromCell + 1)
+              }
+            }
+            if (newY <= cols - 1 && newY >= 0) {
+              fullArea(x)(newY).isVisible = true
+              if (fullArea(x)(newY).isSafe == 0) {
+                fullArea(x)(newY).isSafe = distFromCell + 1
+                safe(x, newY, distFromCell + 1)
+              }
+            }
+            if ((x - 1) >= 0 && y - 1 >= 0) {
+              fullArea(x - 1)(y - 1).isVisible = true
+              if (fullArea(x - 1)(y - 1).count == 0 && !fullArea(x - 1)(y - 1).isVisible) {
+                safe(x - 1, y - 1, distFromCell)
+              }
+            }
+            if ((x - 1) >= 0 && y + 1 <= 9) {
+              fullArea(x - 1)(y + 1).isVisible = true
+              if (fullArea(x - 1)(y + 1).count == 0 && !fullArea(x - 1)(y + 1).isVisible) {
+                safe(x - 1, y + 1, distFromCell)
+              }
+            }
+            if ((x + 1) <= 9 && y - 1 >= 0) {
+              fullArea(x + 1)(y - 1).isVisible = true
+              if (fullArea(x + 1)(y - 1).count == 0 && !fullArea(x + 1)(y - 1).isVisible) {
+                safe(x + 1, y - 1, distFromCell)
+              }
+            }
+            if ((x + 1) <= 9 && y + 1 <= 9) {
+              fullArea(x + 1)(y + 1).isVisible = true
+              if (fullArea(x + 1)(y + 1).count == 0 && !fullArea(x + 1)(y + 1).isVisible) {
+                safe(x + 1, y + 1, distFromCell)
+              }
+            }
+
+          }
+
+        }
+      }
+    }
+  }
+
+  //displays the game's window
+  def display(): Unit ={
+    var i: Int = -1
+    var j: Int = -1
+
+    gamewindow.setColor(Color.gray)
+    gamewindow.drawString(225,40 , "CodeSweeper",Color.DARK_GRAY, 30)
+
+    for (row <- fullArea) {
+      i += 1
+      for (cell <- row) {
+        j += 1
+
+        if (cell.flag) {
+          gamewindow.setColor(Color.gray)
+          gamewindow.drawFillRect(45 + 50 * j + 5 * j, 45 + 50 * i + i * 5, width = 50, height = 50)
+          gamewindow.drawString(60 + 50 * j + j * 5, 80 + 50 * i + 5 * i, "🚩", Color.black, 30)
+        }
+        else if (!cell.isVisible) {
+          gamewindow.setColor(Color.black)
+          gamewindow.drawFillRect(45 + 50 * j + 5 * j, 45 + 50 * i + i * 5, width = 50, height = 50)
+        }
+        else {
+          gamewindow.setColor(Color.gray)
+          gamewindow.drawFillRect(45 + 50 * j + 5 * j, 45 + 50 * i + i * 5, width = 50, height = 50)
+          gamewindow.drawString(60 + 50 * j + j * 5, 80 + 50 * i + 5 * i, s"${cell.count}", Color.black, 30)
+        }
+      }
+      j = -1
+    }
+    i = 0
+  }
+
+  //Winning conditions
   def win(): Boolean = {
     var win : Boolean = false
     var count : Int = 0
@@ -133,104 +185,61 @@ object Minesweeper extends App {
     win
   }
 
-  val rows: Int = 10
-  val cols: Int = 10
-  var nbmines : Int = 10
-  var fullArea: Array[Array[Cell]] = createArray(rows, cols)
-  var nbturn : Int =1
-  var uncvrx: Int = 0
-  var uncvry: Int = 0
-  var flag: Char ='0'
-  placeMines()
-  var posx: Int = 0
-  var posy: Int = 0
 
-  val gamewindow = new FunGraphics(645, 645, "Ba1 Project, Minesweeper Se7en")
-
-
+  //Waiting for User to use the Mouse to flag or discover a tile in order to get the button + the position
   gamewindow.addMouseListener(new MouseAdapter() {
-
     override def mouseClicked(e: MouseEvent): Unit = {
       val event = e
-
+      Button = e.getButton
       // Get the mouse position from the event
       posy = event.getX
       posx = event.getY
-
-      println(s"Mouse position $posx - $posy")
 
       if (posx >= 45 && posx <= 590 && posy >= 45 && posy <= 590) {
         uncvrx = (posx - 45) / 55
         uncvry = (posy - 45) / 55
       }
-      println(s"Tile position $uncvrx - $uncvry")
     }
   })
 
+  //preparing the game's grid with tiles + placing mines
+  var fullArea: Array[Array[Cell]] = createArray(rows, cols)
+  placeMines()
+
+  //Beginning of the game loop
   var game : Boolean = true
   while (game) {
-    var i: Int = -1
-    var j : Int = -1
 
-    println(s"Tile position $uncvrx - $uncvry")
-    for (row <- fullArea) {
-      i+=1
-      for (cell <- row) {
-        j+=1
+    display()
 
-        if (cell.flag) {
-          gamewindow.setColor(Color.gray)
-          gamewindow.drawFillRect(45 + 50 * j + 5 * j, 45 + 50 * i + i * 5, width = 50, height = 50)
-          gamewindow.drawString(60 + 50 * j + j * 5,80 + 50 * i + 5 * i,"🚩",Color.black,30)
-        }
-        else if (!cell.isVisible) {
-          gamewindow.setColor(Color.black)
-          gamewindow.drawFillRect(45 + 50 * j + 5 * j, 45 + 50 * i + i * 5, width = 50, height = 50)
-        }
-        else {
-          gamewindow.setColor(Color.gray)
-          gamewindow.drawFillRect(45 + 50 * j + 5 * j, 45 + 50 * i + i * 5, width = 50, height = 50)
-          gamewindow.drawString(60 + 50 * j + j * 5,80 + 50 * i + 5 * i,s"${cell.count}",Color.black,30)
-        }
-      }
-      j= -1
-    }
-    i =0
-    if (game) {
-
-      if (fullArea(uncvrx)(uncvry).flag){
-        println("remove flag? y/n")
-      }
-      else {
-        println("reveal or put flag? r/f")
-      }
-      flag = Input.readChar()
-
-    }
-
-
-    if (flag == 'r') {
+    if (Button == 1) {
       fullArea(uncvrx)(uncvry).isVisible = true
 
       if (nbturn == 1 || fullArea(uncvrx)(uncvry).count == 0) {
         uncover(uncvrx, uncvry)
       }
       else if (fullArea(uncvrx)(uncvry).isMine && fullArea(uncvrx)(uncvry).isVisible) {
-        println("You hit a mine! Game over!")
+        display()
+        gamewindow.setColor(Color.gray)
+        gamewindow.drawString(225, 630, "You hit a mine! Game over!", Color.DARK_GRAY, 30)
         game = false
       }
-
+      Button = 0
     }
 
-    else if (flag == 'f'){
+    else if (Button == 3 && !fullArea(uncvrx)(uncvry).flag){
       fullArea(uncvrx)(uncvry).flag = true
+      Button =0
     }
-    else if (flag == 'y'){
+    else if (Button == 3 && fullArea(uncvrx)(uncvry).flag){
       fullArea(uncvrx)(uncvry).flag = false
+      Button =0
     }
 
     if (win()) {
-      println("Congratulations! You won!")
+      gamewindow.setColor(Color.gray)
+      gamewindow.drawString(225, 630, "Congratulations! You won!", Color.DARK_GRAY, 30)
+      display()
       game = false
     }
     gamewindow.syncGameLogic(60)
